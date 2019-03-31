9d6d44605a975f71efacd8703fe9499bc8df1a57
Christopher Biscardi <chris@christopherbiscardi.com>
2019-03-31 11:32:39 -0700

    Update gen-mdx.js

--- packages/gatsby-mdx/utils/gen-mdx.js
+++ packages/gatsby-mdx/utils/gen-mdx.js
@@ -147,10 +147,7 @@ module.exports = async function genMDX({
   results.scopeIdentifiers = identifiers;
   // TODO: be more sophisticated about these replacements
   results.body = result.code
-    .replace(
-      /export\s*{\s*MDXContent\s+as\s+default\s*};?/,
-      "return MDXContent;"
-    )
+    .replace(/export\s*{\s*MDXContent\s+as\s+default\s*};?/, "return MDXContent;")
     .replace(/\nexport /g, "\n");
 
   /* results.html = renderToStaticMarkup(
